e7483ae93557ea3767520d5ed67cc0e934462672
alexey-logunov <al_logunov@mail.ru>
2021-09-29 13:12:58 +0300

    modified views.py, urls.py, serializers.py

--- api/core/views.py
+++ api/core/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
-from .serializers import PostSerializer, TagSerializer
+from .serializers import PostSerializer, TagSerializer, ContactSerailizer
 from .models import Post
 from rest_framework.response import Response
 from rest_framework import permissions, pagination, generics
 from taggit.models import Tag
+from rest_framework.views import APIView
+from django.core.mail import send_mail
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -42,3 +44,25 @@ class TagView(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class AsideView(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-id')[:5]
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class FeedBackView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ContactSerailizer
+
+    def post(self, request, *args, **kwargs):
+        serializer_class = ContactSerailizer(data=request.data)
+        if serializer_class.is_valid():
+            data = serializer_class.validated_data
+            name = data.get('name')
+            from_email = data.get('email')
+            subject = data.get('subject')
+            message = data.get('message')
+            send_mail(f'От {name} | {subject}', message, from_email, ['al_logunov@mail.ru'])
+            return Response({"success": "Sent"})
